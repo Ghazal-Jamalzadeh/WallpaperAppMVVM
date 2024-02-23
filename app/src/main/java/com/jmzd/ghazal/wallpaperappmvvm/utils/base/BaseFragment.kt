7a98f06c66b0c845005ab92d203bbaa313884794
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-02-23 18:35:14 +0330

    #405 Handle viewBinding in BaseFragment

--- app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/utils/base/BaseFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/utils/base/BaseFragment.kt
@@ -2,17 +2,25 @@ package com.jmzd.ghazal.wallpaperappmvvm.utils.base
 
 import android.os.Build
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.viewbinding.ViewBinding
 import com.jmzd.ghazal.wallpaperappmvvm.R
 import com.jmzd.ghazal.wallpaperappmvvm.utils.network.NetworkChecker
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-abstract class BaseFragment: Fragment() {
+abstract class BaseFragment<T : ViewBinding> : Fragment() {
+
+    //Binding
+    protected abstract val bindingInflater: (inflater: LayoutInflater) -> T
+    private var _binding: T? = null
+    protected val binding: T get() = requireNotNull(_binding)
 
     @Inject
     lateinit var networkChecker: NetworkChecker
@@ -20,6 +28,10 @@ abstract class BaseFragment: Fragment() {
     //Other
     var isNetworkAvailable = true
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = bindingInflater.invoke(inflater)
+        return binding.root
+    }
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -33,5 +45,10 @@ abstract class BaseFragment: Fragment() {
                 }
             }
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
