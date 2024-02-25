651a2288a3fe56dadff779c1280b435cb11333e3
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-02-25 15:15:08 +0330

    #406 Splash: binding

--- app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/ui/splash/SplashFragment.kt
@@ -1,9 +1,22 @@
 package com.jmzd.ghazal.wallpaperappmvvm.ui.splash
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import com.jmzd.ghazal.wallpaperappmvvm.databinding.FragmentSplashBinding
+import com.jmzd.ghazal.wallpaperappmvvm.utils.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SplashFragment : Fragment() {
+class SplashFragment : BaseFragment<FragmentSplashBinding>() {
+
+    //binding
+    override val bindingInflater: (inflater: LayoutInflater) -> FragmentSplashBinding
+        get() = FragmentSplashBinding::inflate
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {  }
+    }
 
 }
