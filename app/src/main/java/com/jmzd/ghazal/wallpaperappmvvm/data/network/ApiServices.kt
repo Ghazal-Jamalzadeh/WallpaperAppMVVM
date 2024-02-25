0a7a0fe023e36cc0aa85f4cc2ad4b5126cb6a4dc
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-02-25 14:53:33 +0330

    #406 Splash: api services

--- app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/data/network/ApiServices.kt
+++ app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/data/network/ApiServices.kt
@@ -1,4 +1,11 @@
 package com.jmzd.ghazal.wallpaperappmvvm.data.network
 
+import com.jmzd.ghazal.wallpaperappmvvm.data.model.splash.ResponseRandom
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface ApiServices {
+
+    @GET("/photos/random")
+    suspend fun getRandomPhoto() : Response<ResponseRandom>
 }
