43d685c0c9c59c87bed80bf047cedad54478ed45
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-02-25 16:17:26 +0330

    #407 Home - NewestPhotos : api services

--- app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/data/network/ApiServices.kt
+++ app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/data/network/ApiServices.kt
@@ -1,11 +1,15 @@
 package com.jmzd.ghazal.wallpaperappmvvm.data.network
 
+import com.jmzd.ghazal.wallpaperappmvvm.data.model.home.ResponsePhotos
 import com.jmzd.ghazal.wallpaperappmvvm.data.model.splash.ResponseRandom
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiServices {
 
-    @GET("/photos/random")
+    @GET("photos/random")
     suspend fun getRandomPhoto() : Response<ResponseRandom>
+
+    @GET("photos")
+    suspend fun getNewestPhotos() : Response<ResponsePhotos>
 }
