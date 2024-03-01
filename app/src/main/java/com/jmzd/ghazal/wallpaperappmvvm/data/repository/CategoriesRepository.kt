6f2ec33336f6ce192f6f3e578a6f74a5bbbb69fe
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-03-01 17:28:15 +0330

    #410 Categories : repository

--- app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/data/repository/CategoriesRepository.kt
+++ app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/data/repository/CategoriesRepository.kt
@@ -1,8 +1,17 @@
 package com.jmzd.ghazal.wallpaperappmvvm.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.jmzd.ghazal.wallpaperappmvvm.data.network.ApiServices
+import com.jmzd.ghazal.wallpaperappmvvm.ui.categories.CategoriesPagingSource
 import javax.inject.Inject
 
 class CategoriesRepository @Inject constructor(private val api : ApiServices) {
-    suspend fun getCategoriesPhotos(id : String , page : Int ) = api.getCategoryPhotos(id , page )
+
+    fun categoryPhotos(id: String) = Pager(
+        config = PagingConfig(pageSize = 20, maxSize = 100, enablePlaceholders = false),
+        pagingSourceFactory = { CategoriesPagingSource(api, id) }
+    ).liveData
+
 }
