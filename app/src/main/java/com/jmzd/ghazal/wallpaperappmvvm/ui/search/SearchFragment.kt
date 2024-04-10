b571e625085825d28b9702a3e89a79648f7c4b86
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-04-10 18:47:34 +0430

    #412 Search : fragment completed

--- app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/ui/search/SearchFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/ui/search/SearchFragment.kt
@@ -1,11 +1,24 @@
 package com.jmzd.ghazal.wallpaperappmvvm.ui.search
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.jmzd.ghazal.wallpaperappmvvm.R
 import com.jmzd.ghazal.wallpaperappmvvm.databinding.FragmentSearchBinding
 import com.jmzd.ghazal.wallpaperappmvvm.utils.base.BaseFragment
+import com.jmzd.ghazal.wallpaperappmvvm.utils.base.LoadMoreAdapter
+import com.jmzd.ghazal.wallpaperappmvvm.utils.setStatusBarIconsColor
+import com.jmzd.ghazal.wallpaperappmvvm.viewmodel.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding>() {
@@ -14,11 +27,80 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>() {
     override val bindingInflater: (inflater: LayoutInflater) -> FragmentSearchBinding
         get() = FragmentSearchBinding::inflate
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.apply {
+    //viewModel
+    private val viewModel by viewModels<SearchViewModel>()
 
+    //adapter
+    @Inject
+    lateinit var searchAdapter: SearchAdapter
+
+    //args
+    private val args by navArgs<SearchFragmentArgs>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //Args
+        args.let {
+            //Call api
+            viewModel.updateQuery(it.query)
         }
     }
 
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Set color for status bar icons
+        requireActivity().setStatusBarIconsColor(true)
+        //InitViews
+        binding.apply {
+            //Back
+            backImg.setOnClickListener { findNavController().popBackStack() }
+            //Title
+            args.let {
+                searchTitle.text = "${it.query} ${getString(R.string.photos)}"
+            }
+        }
+        //Load data
+        loadData()
+        //Init functions
+        initRecyclerView()
+        loadDataStates()
+    }
+
+    private fun loadData() {
+        viewModel.searchPhotos.observe(viewLifecycleOwner) {
+            searchAdapter.submitData(lifecycle, it)
+        }
+    }
+
+    private fun initRecyclerView() {
+        binding.searchList.apply {
+            layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+            adapter = searchAdapter.withLoadStateFooter(LoadMoreAdapter { searchAdapter.retry() })
+        }
+        //Click
+        searchAdapter.setOnItemClickListener {
+//            val direction = SearchFragmentDirections.actionToDetail(it)
+//            findNavController().navigate(direction)
+        }
+    }
+
+    private fun loadDataStates() {
+        searchAdapter.addLoadStateListener { state ->
+            binding.apply {
+                loading.isVisible = state.source.refresh is LoadState.Loading
+                searchList.isVisible = state.source.refresh is LoadState.NotLoading
+                //Empty
+                if (state.source.refresh is LoadState.NotLoading &&
+                    state.append.endOfPaginationReached && searchAdapter.itemCount < 1
+                ) {
+                    emptyLay.isVisible = true
+                    searchList.isVisible = false
+                } else {
+                    emptyLay.isVisible = false
+                    searchList.isVisible = true
+                }
+            }
+        }
+    }
 }
