55cd89020f9f9a3d075b476e5ddeb6f896221794
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-04-10 14:40:00 +0430

    #411 Categories : fragment - call api

--- app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/ui/categories/CategoriesFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/ui/categories/CategoriesFragment.kt
@@ -4,11 +4,13 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.jmzd.ghazal.wallpaperappmvvm.R
 import com.jmzd.ghazal.wallpaperappmvvm.databinding.FragmentCategoriesBinding
 import com.jmzd.ghazal.wallpaperappmvvm.utils.base.BaseFragment
+import com.jmzd.ghazal.wallpaperappmvvm.viewmodel.CategoriesViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,8 +20,20 @@ class CategoriesFragment : BaseFragment<FragmentCategoriesBinding>() {
     override val bindingInflater: (inflater: LayoutInflater) -> FragmentCategoriesBinding
         get() = FragmentCategoriesBinding::inflate
 
+    //viewModel
+    private val viewModel by viewModels<CategoriesViewModel>()
+
     //args
     private val args by navArgs<CategoriesFragmentArgs>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //Args
+        args.let {
+            //Call api
+            viewModel.updateCategoryId(it.topicId)
+        }
+    }
 
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
