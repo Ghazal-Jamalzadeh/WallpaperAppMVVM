2ca0016d143dae3478e99b195d3e4e8264f84226
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-04-10 18:04:22 +0430

    #412 Search : api services

--- app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/data/network/ApiServices.kt
+++ app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/data/network/ApiServices.kt
@@ -2,6 +2,7 @@ package com.jmzd.ghazal.wallpaperappmvvm.data.network
 
 import com.jmzd.ghazal.wallpaperappmvvm.data.model.home.ResponsePhotos
 import com.jmzd.ghazal.wallpaperappmvvm.data.model.home.ResponseTopics
+import com.jmzd.ghazal.wallpaperappmvvm.data.model.search.ResponseSearch
 import com.jmzd.ghazal.wallpaperappmvvm.data.model.splash.ResponseRandom
 import retrofit2.Response
 import retrofit2.http.GET
@@ -22,4 +23,6 @@ interface ApiServices {
     @GET("topics/{id_or_slug}/photos")
     suspend fun getCategoryPhotos(@Path("id_or_slug") id : String , @Query("page") page : Int) : Response<ResponsePhotos>
 
+    @GET("search/photos")
+    suspend fun searchPhotos(@Query("query") query : String , @Query("page") page : Int) : Response<ResponseSearch>
 }
