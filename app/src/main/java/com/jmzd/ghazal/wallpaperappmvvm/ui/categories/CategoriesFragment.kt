0b8a9cb55fc61563ee8c52b969228c0030350ba5
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-04-10 17:20:17 +0430

    #411 Categories : load more adapter + handle states

--- app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/ui/categories/CategoriesFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/wallpaperappmvvm/ui/categories/CategoriesFragment.kt
@@ -4,14 +4,20 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.jmzd.ghazal.wallpaperappmvvm.R
 import com.jmzd.ghazal.wallpaperappmvvm.databinding.FragmentCategoriesBinding
 import com.jmzd.ghazal.wallpaperappmvvm.utils.base.BaseFragment
+import com.jmzd.ghazal.wallpaperappmvvm.utils.base.LoadMoreAdapter
 import com.jmzd.ghazal.wallpaperappmvvm.viewmodel.CategoriesViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class CategoriesFragment : BaseFragment<FragmentCategoriesBinding>() {
@@ -22,6 +28,10 @@ class CategoriesFragment : BaseFragment<FragmentCategoriesBinding>() {
 
     //viewModel
     private val viewModel by viewModels<CategoriesViewModel>()
+
+    //adapter
+    @Inject
+    lateinit var categoriesAdapter: CategoriesAdapter
 
     //args
     private val args by navArgs<CategoriesFragmentArgs>()
@@ -44,6 +54,38 @@ class CategoriesFragment : BaseFragment<FragmentCategoriesBinding>() {
             //Title
             args.let {
                 categoriesTitle.text = "${it.title} ${getString(R.string.photos)}"
+            }
+        }
+        //load data
+        loadData()
+        //init recycler
+        initRecyclerView()
+
+    }
+
+    private fun loadData() {
+        viewModel.categoriesPhotos.observe(viewLifecycleOwner) {
+            categoriesAdapter.submitData(lifecycle, it)
+        }
+    }
+
+    private fun initRecyclerView() {
+        binding.categoriesList.apply {
+            layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+            adapter = categoriesAdapter.withLoadStateFooter(LoadMoreAdapter { categoriesAdapter.retry() })
+        }
+       // Click
+        categoriesAdapter.setOnItemClickListener {
+//            val direction = CategoriesFragmentDirections.actionToDetail(it)
+//            findNavController().navigate(direction)
+        }
+    }
+
+    private fun loadDataStates() {
+        categoriesAdapter.addLoadStateListener { state ->
+            binding.apply {
+                loading.isVisible = state.source.refresh is LoadState.Loading
+                categoriesList.isVisible = state.source.refresh is LoadState.NotLoading
             }
         }
     }
